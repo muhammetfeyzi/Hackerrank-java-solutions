f2f64f9bf4a47a960f902599f3e53339f17b6bb8
Muhammet Feyzi SAĞLAM <muhammetfeyzi@gmail.com>
2021-10-12 23:12:15 +0300

    Add files via upload
    Given a double-precision number, , denoting an amount of money, use the NumberFormat class' getCurrencyInstance method to convert  into the US, Indian, Chinese, and French currency formats. Then print the formatted values as follows:
    
    US: formattedPayment
    India: formattedPayment
    China: formattedPayment
    France: formattedPayment

---
+++ CurrencyFormatter.java
@@ -0,0 +1,39 @@
+package com.mfs;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+/*
+*
+* @Author Muhammet Feyzi Sağlam
+* */
+public class CurrencyFormatter {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double payment = scan.nextDouble();
+        scan.close();
+        //variable decleration but just not initilization any adress on memory
+        String us ,india,china,france;
+
+        NumberFormat nfUs = NumberFormat.getCurrencyInstance(new Locale("en", "US"));
+        us = nfUs.format(payment);
+        System.out.println("US: " + us);
+
+
+        NumberFormat nfInd = NumberFormat.getCurrencyInstance(new Locale("en","IN"));
+        india = nfInd.format(payment);
+        System.out.println("India: " + india);
+
+
+        NumberFormat nfChnia = NumberFormat.getCurrencyInstance(new Locale("zh", "CN"));
+        china = nfChnia.format(payment);
+        System.out.println("China: " + china);
+
+
+        NumberFormat nfFrance = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        france = nfFrance.format(payment);
+        System.out.println("France: " + france);
+
+    }
+}
